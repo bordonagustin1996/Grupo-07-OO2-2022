a6a22df181dcf731106d98b9a950f2c34acde13f
abordon <bordonagustin1996@gmail.com>
2022-05-14 18:23:53 -0300

    Initial commit

---
+++ src/main/java/com/unla/Grupo07OO22022/Grupo07Oo22022Application.java
@@ -0,0 +1,13 @@
+package com.unla.Grupo07OO22022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo07Oo22022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo07Oo22022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/unla/Grupo07OO22022/Grupo07Oo22022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.Grupo07OO22022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo07Oo22022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

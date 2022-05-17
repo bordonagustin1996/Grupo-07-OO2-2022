f07063e4ab019eb3efbe8cd9007314234fe2b3f8
abordon <bordonagustin1996@gmail.com>
2022-05-17 02:09:02 -0300

    Elimina helper home/index ya que no se usa

--- src/main/java/com/unla/Grupo07OO22022/helpers/ViewRouteHelper.java
+++ src/main/java/com/unla/Grupo07OO22022/helpers/ViewRouteHelper.java
@@ -1,10 +1,7 @@
 package com.unla.Grupo07OO22022.helpers;
 
 public class ViewRouteHelper {
-	
-	//HOME
-	public final static String INDEX = "home/index";
-	
+		
 	//USER
 	public final static String USER_NEW = "user/new";
 	public final static String USER_UPDATE = "user/update";

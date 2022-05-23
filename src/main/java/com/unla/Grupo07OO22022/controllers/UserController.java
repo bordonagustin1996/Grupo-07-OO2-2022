20cba59131c1193091accbe394b5795488353071
gmajuria <gmajuria@gmail.com>
2022-05-23 18:16:28 -0300

    Modifica seguridad

--- src/main/java/com/unla/Grupo07OO22022/controllers/UserController.java
+++ src/main/java/com/unla/Grupo07OO22022/controllers/UserController.java
@@ -15,9 +15,17 @@ import com.unla.Grupo07OO22022.models.UserModel;
 import com.unla.Grupo07OO22022.services.implementation.UserRoleService;
 import com.unla.Grupo07OO22022.services.implementation.UserService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Controller
 @RequestMapping("/user")
@@ -35,8 +43,7 @@ public class UserController {
 		
 	@GetMapping("")
 	public ModelAndView index() {
-		ModelAndView mAV = new ModelAndView("user/index");	
-		//mAV.addObject("users", this.userService.getAll());
+		ModelAndView mAV = new ModelAndView("user/index");
 		mAV.addObject("users", this.userService.findByEnabled(true));
 		return mAV;
 	}
@@ -70,7 +77,7 @@ public class UserController {
 	}
 	
 	@PostMapping("/update")
-	public RedirectView update(@ModelAttribute("person") UserModel userModel) {
+	public RedirectView update(@ModelAttribute("user") UserModel userModel) {
 		User user = modelMapper.map(userModel, User.class);
 		if(userModel.getId() > 0) {
 			User userOld = this.userService.findById(userModel.getId());
@@ -80,6 +87,13 @@ public class UserController {
 			user.setEmail(userOld.getEmail());
 		}
 		this.userService.insertOrUpdate(user);
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth.getName().equals(userModel.getUsername())) {
+			List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+			authorities.add(new SimpleGrantedAuthority(userModel.getUserRole().getName()));
+			Authentication newAuth = new UsernamePasswordAuthenticationToken(auth.getPrincipal(), auth.getCredentials(), authorities);
+			SecurityContextHolder.getContext().setAuthentication(newAuth);
+		}
 		return new RedirectView(ViewRouteHelper.USER_ROOT);
 	}
 	

43420dfa3671db23a3d749f8e1046dbac571a326
IgnacioIA <ignaciocruz1@hotmail.com>
2022-05-23 21:04:38 -0300

    Controller Career

--- src/main/java/com/unla/Grupo07OO22022/controllers/CareerController.java
+++ src/main/java/com/unla/Grupo07OO22022/controllers/CareerController.java
@@ -1,20 +1,23 @@
 package com.unla.Grupo07OO22022.controllers;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.unla.Grupo07OO22022.services.IOrderNoteService;
+import com.unla.Grupo07OO22022.services.implementation.CareerService;
 
 @Controller
-@RequestMapping("/orderNote")
-public class OrderNoteController {
-
+@RequestMapping("/Career")
+public class CareerController {
+	
 	@Autowired
-	@Qualifier("orderNoteService")
-	private IOrderNoteService orderNoteService;
+	@Qualifier("careerService")
+	private CareerService careerService;
+	
+	private ModelMapper modelMapper = new ModelMapper();
 	
 	
-	
+
 }
